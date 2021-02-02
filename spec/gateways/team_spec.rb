9fe9f90abdae4dfdfdce04984836ae8b0d0dd9db
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-02-02 14:27:36 +0000

    Add `continuously_deployed_apps` property to `Gateways::Team`
    We will add this property to the Developer Docs prior to merge.
    The `continuously_deployed_apps` will be used to edit the
    Dependabot Slack messages, drawing attention to PRs which can be
    merged & deployed with one click, thanks to the Continous
    Deployment process. This should mean developers are more open to
    merging Dependabot PRs.

--- spec/gateways/team_spec.rb
+++ spec/gateways/team_spec.rb
@@ -1,3 +1,5 @@
+require_relative "../spec_helper"
+
 describe Gateways::Team do
   context "Given no teams" do
     before do
@@ -25,6 +27,7 @@ describe Gateways::Team do
       expect(result.count).to eq(1)
       expect(result.first.team_name).to eq("govuk-developers")
       expect(result.first.applications).to eq(%w[asset-manager])
+      expect(result.first.continuously_deployed_apps).to eq(%w[asset-manager])
     end
   end
 
@@ -43,6 +46,7 @@ describe Gateways::Team do
         expect(result.count).to eq(1)
         expect(result.first.team_name).to eq("govuk-pub-workflow")
         expect(result.first.applications).to eq(%w[asset-manager])
+        expect(result.first.continuously_deployed_apps).to eq(%w[asset-manager])
       end
     end
 
@@ -60,6 +64,7 @@ describe Gateways::Team do
         expect(result.count).to eq(1)
         expect(result.first.team_name).to eq("govuk-platform-health")
         expect(result.first.applications).to eq(%w[asset-manager asset-uploader])
+        expect(result.first.continuously_deployed_apps).to eq(%w[asset-manager])
       end
     end
   end
@@ -79,9 +84,11 @@ describe Gateways::Team do
       expect(result.count).to eq(2)
       expect(result.first.team_name).to eq("govuk-platform-health")
       expect(result.first.applications).to eq(%w[publisher frontend])
+      expect(result.first.continuously_deployed_apps).to eq([])
 
       expect(result.last.team_name).to eq("govuk-searchandnav")
       expect(result.last.applications).to eq(%w[government-frontend])
+      expect(result.last.continuously_deployed_apps).to eq(%w[government-frontend])
     end
   end
 end
